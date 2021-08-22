8fe43f5a424636f8e62844df59b5e0f81a6c42b1
OppoCommando <48214348+OppoCommando@users.noreply.github.com>
2021-08-22 22:52:50 +0530

    Welcome Page Design

---
+++ app/src/main/java/com/myself/mynotebook/activities/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.myself.mynotebook.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myself.mynotebook.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
