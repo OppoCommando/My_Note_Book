c6ea544aee498fdf5c79e032cba6bdae6fd0a1ad
OppoCommando <48214348+OppoCommando@users.noreply.github.com>
2021-08-24 23:06:15 +0530

    Login page add

---
+++ app/src/main/java/com/myself/mynotebook/activities/UaerCredetialsActivity.kt
@@ -0,0 +1,12 @@
+package com.myself.mynotebook.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myself.mynotebook.R
+
+class UaerCredetialsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uaer_credetials)
+    }
+}
