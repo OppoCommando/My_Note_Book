018861cdfdf84404b7d3d0d3efccf84fda9f83b1
OppoCommando <48214348+OppoCommando@users.noreply.github.com>
2021-09-10 22:44:56 +0530

    EditText onChange , Password hide & show implement

--- app/src/main/java/com/myself/mynotebook/fragments/LoginFragment.kt
+++ app/src/main/java/com/myself/mynotebook/fragments/LoginFragment.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,7 +26,9 @@ class LoginFragment : Fragment() {
     var et_password: AppCompatEditText?=null
     var show_pass_btn: AppCompatImageView?=null
     var userPhone:String?=null
+    var userCheckPhone:String?=null
     var userPassword:String?=null
+    var isPasswordHide:Boolean?=true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -35,7 +39,7 @@ class LoginFragment : Fragment() {
         val view: View = inflater.inflate(R.layout.fragment_login, container, false)
         instance(view)
         clickEvent()
-       // textChangeEvent()
+        textChangeEvent()
         return view;
     }
 
@@ -58,17 +62,59 @@ class LoginFragment : Fragment() {
 
             doUserLogin()
         })
+
+        show_pass_btn?.setOnClickListener(View.OnClickListener {
+            setPasswordHideShow()
+        })
     }
 
-  /*  private fun textChangeEvent() {
-        et_password?.addTextChangedListener(object :TextWatcher{
+    private fun setPasswordHideShow() {
+        if(isPasswordHide==false){
+            et_password?.transformationMethod = PasswordTransformationMethod.getInstance()
+            isPasswordHide=true
+            show_pass_btn?.setImageResource(R.drawable.ic_eye_line)
+        }
+        else {
+            et_password?.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            show_pass_btn?.setImageResource(R.drawable.ic_eye_off_line)
+            isPasswordHide=false
+        }
+    }
+
+    private fun textChangeEvent() {
+
+
+        var count:Int=0
+           et_phone?.addTextChangedListener(object :TextWatcher{
             override fun beforeTextChanged(changePassword: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onTextChanged(changePassword: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                setEditBackGraund("Phone")
+
+            }
+
+            override fun afterTextChanged(changePassword: Editable?) {
+
+            }
+
+        })
+           et_password?.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(changePassword: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                userCheckPhone=et_phone?.text.toString()
+                if(userCheckPhone?.length==0 && count==0){
+                    setErrorMsg("Phone","Enter Phone Number!!!")
+                    count = 1
+                }
+            }
+
+            override fun onTextChanged(changePassword: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                setEditBackGraund("Password")
                 if(changePassword?.length==0){
+
                     show_pass_btn?.visibility=View.GONE
+                    //count = 0
                 }
                 else{
                     show_pass_btn?.visibility=View.VISIBLE
@@ -76,15 +122,15 @@ class LoginFragment : Fragment() {
             }
 
             override fun afterTextChanged(changePassword: Editable?) {
-                TODO("Not yet implemented")
+
             }
 
         })
-    }*/
+    }
 
     private fun doUserLogin() {
-      userPhone=et_phone?.text.toString()
-      userPassword=et_password?.text.toString()
+        userPassword=et_password?.text.toString()
+        userPhone=et_phone?.text.toString()
       var isOkay = validFields()
       if(isOkay==true){
           var clickintent = Intent(mActivity, MainActivity::class.java)
@@ -95,21 +141,17 @@ class LoginFragment : Fragment() {
     private fun validFields(): Boolean? {
         var isCheck:Boolean=false
         if(userPhone?.isEmpty() == true && userPassword?.isEmpty()==true){
-            mActivity?.setSnackBar("Enter All Fields!!!")
-            et_phone?.setBackgroundResource(R.drawable.et_errorcustom)
-            et_password?.setBackgroundResource(R.drawable.et_errorcustom)
+            setErrorMsg("All","Enter All Fields!!!")
         }
        else if(userPhone?.isEmpty() == true){
-            mActivity?.setSnackBar("Enter Phone Number")
-            et_phone?.setBackgroundResource(R.drawable.et_errorcustom)
+            setErrorMsg("Phone","Enter Phone Number!!!")
         }
         else if(userPhone?.length!=10 ){
-            mActivity?.setSnackBar("Enter Valid Phone Number")
-            et_phone?.setBackgroundResource(R.drawable.et_errorcustom)
+
+            setErrorMsg("Phone","Enter Valid Phone Number!!!")
         }
         else if(userPassword?.isEmpty() == true){
-            mActivity?.setSnackBar("Enter Password")
-            et_password?.setBackgroundResource(R.drawable.et_errorcustom)
+            setErrorMsg("Password","Enter Password!!!")
         }
         else{
             isCheck=true
@@ -121,6 +163,35 @@ class LoginFragment : Fragment() {
 
 
        return isCheck
+    }
+
+    private fun setErrorMsg(indicator:String,Msg:String){
+        if(indicator == "Phone"){
+            mActivity?.setSnackBar(Msg)
+            et_phone?.setBackgroundResource(R.drawable.et_errorcustom)
+        }
+        else if(indicator=="Password"){
+            mActivity?.setSnackBar(Msg)
+            et_password?.setBackgroundResource(R.drawable.et_errorcustom)
+        }
+        else{
+            mActivity?.setSnackBar(Msg)
+            et_phone?.setBackgroundResource(R.drawable.et_errorcustom)
+            et_password?.setBackgroundResource(R.drawable.et_errorcustom)
+        }
+    }
+    private fun setEditBackGraund(indicator:String){
+        if(indicator=="Phone"){
+            et_phone?.setBackgroundResource(R.drawable.et_custom)
+        }
+        else if(indicator=="Password"){
+            et_password?.setBackgroundResource(R.drawable.et_custom)
+        }
+        else{
+            et_phone?.setBackgroundResource(R.drawable.et_custom)
+            et_password?.setBackgroundResource(R.drawable.et_custom)
+        }
+
     }
 
 }
